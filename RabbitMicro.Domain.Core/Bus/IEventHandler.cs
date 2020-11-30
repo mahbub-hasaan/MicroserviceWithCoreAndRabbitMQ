64a14421dd6dc404b321fe67f700dc2703afb9c7
shakil-adnan <shakil00095@gmail.com>
2020-11-30 18:33:55 +0600

    initial build

--- RabbitMicro.Domain.Core/Bus/IEventHandler.cs
+++ RabbitMicro.Domain.Core/Bus/IEventHandler.cs
@@ -9,9 +9,11 @@ namespace RabbitMicro.Domain.Core.Bus
     public interface IEventHandler<in TEvent> where TEvent : Event
     {
         Task Handle(TEvent @event);
-        public interface IEventHandler
-        {
+        
+    }
 
-        }
+    public interface IEventHandler
+    {
+
     }
 }
